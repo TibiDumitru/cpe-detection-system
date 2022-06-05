41d177094783393c095a444609cf39d798b64b57
alexandraminea <alexandramineaacs@gmail.com>
2022-06-05 18:35:44 +0300

    add update entries and first query

--- cpe_match.py
+++ cpe_match.py
@@ -20,55 +20,57 @@ def load_cpe_data(save_csv_file):
     df = pd.read_xml(CPE_DICT_URL, xpath=".//doc:cpe-item", namespaces={'doc': 'http://cpe.mitre.org/dictionary/2.0'})
     if save_csv_file:
         df.to_csv('cpes.csv')
+    return df
+
+def df_from_file():
+    df = pd.read_csv('cpes.csv')
 
     return df
 
-def rec_to_actions(df):
-    import json
-    for record in df.to_dict(orient="records"):
-        yield ('{ "index" : { "_index" : "cpes", "_type" : "record" }}')
-        yield (json.dumps(record, default=int))
+def gendata(df):
+    for i in range(len(df)):
+        yield {
+            "_index": "cpes",
+            # "_op_type": 'create',
+            # '_id': i,
+            "cpe": df['name'].values[i], 'title': df['title'].values[i],
+            # 'doc': {"cpe": df['name'].values[i], 'title': df['title'].values[i],}
+        }
+
+def create_index():
+    request_body = {
+        "settings" : {
+            "number_of_shards": 2,
+            "number_of_replicas": 1
+        },
+        "mappings": {
+            "properties": {
+                "cpe": { "type": "text" },
+                "title": { "type": "text" },
+            }
+        }
+	}
+    print("creating 'cpes' index")
+    es.indices.create(index = 'cpes', body = request_body)
+
 
 def update_index(df):
+    if not es.indices.exists('cpes'):
+        print("Creating index 'cpes'")
+        create_index()
     
-    # for i in range(len(df)):
-    #     es.index(
-    #     index='cpe-match',
-    #     document={
-    #     'cpe': df['name'].values[i],
-    #     'title': df['title'].values[i]
-    #     })
-
-    # es.indices.refresh(index='cpe-match')
-    # actions = []
-    # counter = 0
-
-    # for i in range(100):#range(len(df)):
-    #     source = {
-    #     'cpe': df['name'].values[i],
-    #     'title': df['title'].values[i]
-    #     }
-    #     action = {
-    #     '_op_type': 'index',
-    #     '_index': 'cpes',
-    #     '_id': counter,
-    #     'doc': source
-    #     }
-    #     actions.append(action)
-    #     counter += counter
-    #     if len(actions) >= 10:
-    #         helpers.bulk(es, actions)
-    #         del actions[0:len(actions)]
-
-    # if len(actions) > 0:
-    r = es.bulk(rec_to_actions(df))
-    print(not r['errors'])
+    helpers.bulk(es, gendata(df))
 
 def main(args):
+
+    if args.CREATE_INDEX:
+        create_index()
+
     if args.UPDATE:
-        df = load_cpe_data(args.CSV_FILE)
-        print('CPE data loaded')
-        update_index(df)
+        # df = load_cpe_data(args.CSV_FILE)
+        # print('CPE data loaded')
+        new_df = df_from_file()
+        update_index(new_df)
         exit(0)
 
     no_results = args.MAX_RESULTS
@@ -82,19 +84,18 @@ def main(args):
         print("The application name has to be specified")
         exit(1)
 
+    es.indices.refresh('cpes')
+    # res = es.cat.count('cpes', params={"format": "json"})
+    # print(res)
+
     print(f"Finding the corresponding CPE for {name}...")
 
-    result = es.search(
-        index='cpes',
-        body={}
-        #'match_all': {}
-        #}
-    )
-
+    result = es.search(body={"query": {"match": {"title":name}}}, index = 'cpes', size=args.MAX_RESULTS)
     print(result['hits']['hits'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--create-index', dest="CREATE_INDEX", help="create index 'cpes'", default=False)
     parser.add_argument('--update', action="store_true", dest="UPDATE", help="update index with the latest CPE dictionary data", default=False)
     parser.add_argument('-n', '--name', dest="NAME", help="the application name", default=None)
     parser.add_argument('--csvfile', action="store_true", dest="CSV_FILE", help="save the csv file containing the cpe dictionary data", default=False)
